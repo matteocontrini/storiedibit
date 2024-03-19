e548521f13b9b0ab9f6f64b7bc2288514db4a959
Matteo Contrini <matteo@contrini.it>
2024-03-19 11:22:00 +0100

    Enable debug mode for admins only

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
-    'debug' => true, // TODO
+    'debug' => false,
+    'ready' => function ($kirby) {
+        return [
+            'debug' => kirby()->user() && kirby()->user()->role()->isAdmin()
+        ];
+    },
     'panel' => [
         'css' => 'assets/css/panel.css',
     ]
