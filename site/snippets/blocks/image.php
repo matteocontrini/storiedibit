5c4f522a7776e98186322aab868d720d6a726505
Matteo Contrini <matteo@contrini.it>
2024-03-19 23:09:57 +0100

    Fix images

---
+++ site/snippets/blocks/image.php
@@ -0,0 +1,33 @@
+<?php
+
+/** @var \Kirby\Cms\Block $block */
+$alt = $block->alt();
+$caption = $block->caption();
+
+$image = $block->image()->toFile();
+$alt = $alt->or($image->alt());
+
+if ($block->location() == 'web') {
+    $src = $url = $block->src()->esc();
+} elseif ($image = $block->image()->toFile()) {
+    $alt = $alt->or($image->alt());
+    $src = $image->resize(640)->url();
+    $url = $image->url();
+}
+
+if ($block->link()->isNotEmpty()) {
+    $url = Str::esc($block->link()->toUrl());
+}
+
+?>
+<figure>
+    <a href="<?= $url ?>" target="_blank">
+        <img src="<?= $src ?>" alt="<?= $alt ?>">
+    </a>
+
+    <?php if ($caption->isNotEmpty()): ?>
+        <figcaption>
+            <?= $caption ?>
+        </figcaption>
+    <?php endif ?>
+</figure>
