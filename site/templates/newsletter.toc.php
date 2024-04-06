d09c445bfb24e19fcc7c9f4955f8cd47e3865e98
Matteo Contrini <matteo@contrini.it>
2024-04-06 14:24:29 +0200

    Update toc script

--- site/templates/newsletter.toc.php
+++ site/templates/newsletter.toc.php
@@ -9,14 +9,32 @@ $kirby->user() && $kirby->user()->role()->isAdmin() || exit();
 
 $blocks = $page->text()->toBlocks();
 
-echo '<div style="font-family: sans-serif; font-size: 17px; line-height: 28px; width: 640px; margin: 0 auto;">';
+echo '<div style="font-family: sans-serif; font-size: 17px; line-height: 28px; width: 640px; margin: 70px auto;">';
+
+$subsections = [];
 
 foreach ($blocks as $block) {
     if ($block->type() === 'newsletter-section') {
-        echo '<br><br><a href="#' . $block->text()->slug() . '">' . $block->text() . '</a>:<br>';
+        if (!empty($subsections)) {
+            for ($i = 0; $i < count($subsections); $i++) {
+                echo '• ' . $subsections[$i] . ($i === count($subsections) - 1 ? '' : ' ');
+            }
+            $subsections = [];
+            echo '</p>';
+        }
+
+        $url = $page->url() . '#' . $block->text()->slug();
+        echo '<p><a href="' . $url . '">' . $block->text() . '</a>:<br>';
     } else if ($block->type() === 'newsletter-subsection') {
-        echo '• ' . $block->text() . ' ';
+        $subsections[] = $block->text();
     }
+}
+
+if (!empty($subsections)) {
+    for ($i = 0; $i < count($subsections); $i++) {
+        echo '• ' . $subsections[$i] . ($i === count($subsections) - 1 ? '' : ' ');
+    }
+    echo '</p>';
 }
 
 echo '</div>';
